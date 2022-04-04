ff60a44be2e5139e1f7129e7911e281a51439e3e
youlookwhat <770413277@qq.com>
2022-04-04 21:35:49 +0800

    add UITabBarController 部分

---
+++ iOS-project/UITabBarController/UITabBarController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITabBarController
+//
+//  Created by 景彬 on 2022/4/4.
+//  Copyright © 2022 景彬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS-project/UITabBarController/UITabBarController/ViewControllerFour.swift
@@ -0,0 +1,32 @@
+//
+//  ViewControllerFour.swift
+//  UITabBarController
+//
+//  Created by 景彬 on 2022/4/4.
+//  Copyright © 2022 景彬. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerFour: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tabBarItem.title = "个人中心"
+        self.view.backgroundColor = UIColor.blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
