6ac5427c69f7e58222e1cded7ecd5202acaadc69
youlookwhat <770413277@qq.com>
2022-04-07 23:19:46 +0800

    add UIScrollView 部分

--- iOS-project/UITabBarController/UITabBarController/ViewControllerTop.swift
+++ iOS-project/UITabBarController/UITabBarController/ViewControllerTop.swift
@@ -13,18 +13,23 @@ class ViewControllerTop: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tabBarItem.title = "滚动视图"
+        self.view.backgroundColor = UIColor.white
+        
+        let sc = UIScrollView(frame: self.view.frame)
+        self.view.addSubview(sc)
+        
+        // 左右滑动的视图
+        let view1 = UIView(frame: self.view.frame)
+        view1.backgroundColor =  UIColor.red
+        let view2 = UIView(frame: CGRect(x: self.view.frame.size.width, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        view2.backgroundColor =  UIColor.black
+        sc.addSubview(view1)
+        sc.addSubview(view2)
+        // 另外设置实际的位置与尺寸
+        sc.contentSize = CGSize(width: self.view.frame.size.width*2, height: self.view.frame.size.height)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
