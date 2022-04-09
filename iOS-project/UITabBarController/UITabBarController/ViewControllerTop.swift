077223bd5b5cc3c60cabf70e24d9a634d84ead7b
youlookwhat <770413277@qq.com>
2022-04-09 22:20:07 +0800

    add UIScrollView

--- iOS-project/UITabBarController/UITabBarController/ViewControllerTop.swift
+++ iOS-project/UITabBarController/UITabBarController/ViewControllerTop.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewControllerTop: UIViewController {
+class ViewControllerTop: UIViewController,UIScrollViewDelegate {
 
+    
+    var   image:UIImageView? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,10 +27,36 @@ class ViewControllerTop: UIViewController {
         view1.backgroundColor =  UIColor.red
         let view2 = UIView(frame: CGRect(x: self.view.frame.size.width, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
         view2.backgroundColor =  UIColor.black
-        sc.addSubview(view1)
-        sc.addSubview(view2)
+//        sc.addSubview(view1)
+//        sc.addSubview(view2)
         // 另外设置实际的位置与尺寸
-        sc.contentSize = CGSize(width: self.view.frame.size.width*2, height: self.view.frame.size.height)
+//        sc.contentSize = CGSize(width: self.view.frame.size.width*2, height: self.view.frame.size.height)
+        // 开启竖直方向的回弹效果
+        sc.alwaysBounceVertical = true
+        // 是否显示滚动条
+        sc.showsVerticalScrollIndicator = false
+        
+        // 开启自动定位分页效果，轮播图使用
+        sc.isPagingEnabled = true
+        
+        sc.delegate = self
+        
+        // 缩放操作
+        image = UIImageView(image: UIImage(named: "home"))
+        image!.frame = self.view.frame
+        sc.addSubview(image!)
+        sc.contentSize = self.view.frame.size
+        
+        sc.minimumZoomScale = 0.5
+        sc.maximumZoomScale = 2
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return image!
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print("当滚动视图滚动时被系统自动调用")
     }
     
 
