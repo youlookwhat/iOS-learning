9818dce5060fda821b3f2687a465fd4450bf2dda
youlookwhat <770413277@qq.com>
2022-04-12 23:30:12 +0800

    add WaterFallLayout UICollectionView 完

--- iOS-project/UITabBarController/UITabBarController/ViewControllerCollectionView.swift
+++ iOS-project/UITabBarController/UITabBarController/ViewControllerCollectionView.swift
@@ -8,13 +8,63 @@
 
 import UIKit
 
-class ViewControllerCollectionView: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ViewControllerCollectionView: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        // 学习UICollectionViewFlowLayout
+//        studyUICollectionViewFlowLayout()
+        
+        // 瀑布流
+        studyWaterFallLayout()
+        
+        
+    }
+    
+    func studyWaterFallLayout(){
+        let layout = WaterFallLayout(itemCount: 30)
+        let collectionView  =  UICollectionView(frame: self.view.frame,collectionViewLayout: layout)
+//        let impl = CollectionViewImp()
+//        collectionView.delegate = impl
+//        collectionView.dataSource = impl
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        
+        collectionView.register(NSClassFromString("UICollectionViewCell"), forCellWithReuseIdentifier: "itemId")
+        self.view.addSubview(collectionView)
+    }
+    
+    
+    
+    class CollectionViewImp :NSObject,UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+        
+        // 分区个数
+       func numberOfSections(in collectionView: UICollectionView) -> Int {
+           return 1
+       }
+    
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return 30
+        }
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemId", for: indexPath)
+            cell.backgroundColor = UIColor(red: CGFloat(arc4random()%255)/255, green: CGFloat(arc4random()%255)/255, blue: CGFloat(arc4random()%255)/255, alpha: 1)
+            return cell
+        }
+        
+    }
+    
+    
+    
+    //------------------------------------------------------------------
+
+    func studyUICollectionViewFlowLayout(){
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.itemSize = CGSize(width: 100, height: 100)
@@ -22,9 +72,21 @@ class ViewControllerCollectionView: UIViewController, UICollectionViewDelegate, 
         cv.delegate = self
         cv.dataSource = self
         cv.register(NSClassFromString("UICollectionViewCell"), forCellWithReuseIdentifier: "itemId")
+        
+        // 流式布局配置
+        // 最小行间距
+        layout.minimumLineSpacing = 30
+        // 最小列间距
+        layout.minimumInteritemSpacing = 100
+        // 头视图尺寸
+        layout.headerReferenceSize = CGSize(width: self.view.frame.size.width, height: 100)
+        layout.footerReferenceSize = CGSize(width: self.view.frame.size.width, height: 100)
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
         self.view.addSubview(cv)
     }
-
+    
+    
     // 分区个数
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -32,18 +94,35 @@ class ViewControllerCollectionView: UIViewController, UICollectionViewDelegate, 
     
     // 分区的item数
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 30
+//        return 10
     }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemId", for: indexPath)
-        cell.backgroundColor = UIColor.red
+        cell.backgroundColor = UIColor(red: CGFloat(arc4random()%255)/255, green: CGFloat(arc4random()%255)/255, blue: CGFloat(arc4random()%255)/255, alpha: 1)
         return cell
     }
+    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemId", for: indexPath)
+//        cell.backgroundColor = UIColor.red
+//        return cell
+//    }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("第\(indexPath.row)个item被点击")
     }
+    
+    
+    // -----------  FlowLayout  流式布局
+    // 可以自定义高度，不过要继承UICollectionViewDelegateFlowLayout
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,sizeForItemAt indexPath:IndexPath) -> CGSize {
+//        if indexPath.row%2==0 {
+//            return CGSize(width: 50, height: 50)
+//        } else {
+//            return CGSize(width: 100, height: 100)
+//        }
+//    }
     
 
     /*
---
+++ iOS-project/UITabBarController/UITabBarController/WaterFallLayout.swift
@@ -0,0 +1,55 @@
+//
+//  WaterFallLayout.swift
+//  UITabBarController
+//
+//  Created by 景彬 on 2022/4/12.
+//  Copyright © 2022 景彬. All rights reserved.
+//
+
+import UIKit
+
+// 瀑布流布局
+class WaterFallLayout: UICollectionViewFlowLayout {
+
+    var itemCount :Int = 0
+    var attributeArray:Array<UICollectionViewLayoutAttributes>?
+    
+    required init?(coder: NSCoder) {
+        itemCount = 0
+        super.init(coder: coder)
+    }
+    init(itemCount:Int){
+        self.itemCount = itemCount
+        super.init()
+    }
+    override func prepare() {
+        super.prepare()
+        self.scrollDirection = .vertical
+        attributeArray = Array<UICollectionViewLayoutAttributes>()
+        let WIDTH = (UIScreen.main.bounds.size.width - self.minimumInteritemSpacing)/2
+        var queueHeight:(one:Int,two:Int) = (0,0)
+        for index in 0..<self.itemCount{
+            let indexPath = IndexPath(item: index, section: 0)
+            let artts = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+            
+            let  height = Int(arc4random()%150+40)
+            var queue = 0
+            if queueHeight.one <= queueHeight.two{
+                queueHeight.one += (height+Int(self.minimumInteritemSpacing))
+                queue = 0
+            } else {
+                queueHeight.two  += (height+Int(self.minimumInteritemSpacing))
+                queue = 1
+            }
+            // 符号左右要有空格
+            let tmpH = (queue == 0) ? (queueHeight.one - height) : (queueHeight.two-height)
+            artts.frame = CGRect(x: (self.minimumInteritemSpacing+WIDTH)*CGFloat(queue), y: CGFloat(tmpH), width: WIDTH, height: CGFloat(height))
+            attributeArray?.append(artts)
+        }
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        return attributeArray
+    }
+    
+}
