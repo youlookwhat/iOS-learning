34c42801d5125adb7ce0abc4ffd2269b4a7c2bea
youlookwhat <770413277@qq.com>
2022-04-14 22:48:04 +0800

    add 使用UIImageView/UIWebView播放动画

--- iOS-project/UITabBarController/UITabBarController/ViewControllerAnimation.swift
+++ iOS-project/UITabBarController/UITabBarController/ViewControllerAnimation.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+// gif使用
+import ImageIO
 
 class ViewControllerAnimation: UIViewController {
 
@@ -18,7 +20,71 @@ class ViewControllerAnimation: UIViewController {
 
         studyAnimal()
         studyTransAnimal()
+        
+        studyGifImage()
+        studyGifWebView()
     }
+    
+    // 使用UIWebView播放动画
+    func studyGifWebView(){
+        let imageView = UIWebView(frame: CGRect(x: 100, y: 610, width: 200, height: 150))
+        self.view.addSubview(imageView)
+        self.playGIFOnWebView(name: "animation", webView: imageView)
+    }
+    
+    func playGIFOnWebView(name:String,webView:UIWebView) {
+        let path = Bundle.main.path(forResource: name, ofType: "gif")
+        let url = URL.init(fileURLWithPath: path!)
+        let imageData = try! Data(contentsOf: url)
+        webView.scrollView.bounces = false
+        webView.backgroundColor = UIColor.clear
+        webView.scalesPageToFit = true
+        webView.load(imageData, mimeType: "image/gif", textEncodingName: "", baseURL: url)
+    }
+    
+    //------------------------------------------
+    
+    
+    // 使用ImageView显示Gif动画
+    func studyGifImage(){
+        let imageView = UIImageView(frame: CGRect(x: 100, y: 450, width: 200, height: 150))
+        self.view.addSubview(imageView)
+        self.playGIFOnImageView(name: "animation", imageView: imageView)
+    }
+    
+    func playGIFOnImageView(name:String,imageView:UIImageView){
+        let path = Bundle.main.path(forResource: name, ofType: "gif")
+        let url = URL.init(fileURLWithPath: path!)
+        let source = CGImageSourceCreateWithURL(url as CFURL, nil)
+        // 图片张数
+        let count = CGImageSourceGetCount(source!)
+        var imageArray = Array<UIImage>()
+        var imageWidth = Array<Int>()
+        var imageHeight = Array<Int>()
+        // 播放时长
+        var time:Int = Int()
+        for index in 0..<count {
+            let image = CGImageSourceCreateImageAtIndex(source!, index, nil)
+            imageArray.append(UIImage(cgImage: image!))
+            let info = CGImageSourceCopyPropertiesAtIndex(source!, index, nil) as! Dictionary<String,AnyObject>
+            let width = Int(info[kCGImagePropertyPixelWidth as String]! as! NSNumber)
+            let height = Int(info[kCGImagePropertyPixelHeight as String]! as! NSNumber)
+            imageWidth.append(width)
+            imageHeight.append(height)
+            let timeDic = info[kCGImagePropertyGIFDictionary as String] as! Dictionary<String,AnyObject>
+            time += Int(timeDic[kCGImagePropertyGIFDelayTime as String] as! NSNumber)
+        }
+        // 重新设置gif图片的宽高
+//        imageView.frame = CGRect(x: 0, y: 100, width: imageWidth[0], height: imageHeight[0])
+        imageView.animationImages = imageArray
+        imageView.animationDuration = TimeInterval(time)
+        imageView.animationRepeatCount = 0
+        imageView.startAnimating()
+    }
+    
+    
+    //------------------------------------------
+    
     
     // 转场动画
     func studyTransAnimal(){
@@ -34,7 +100,7 @@ class ViewControllerAnimation: UIViewController {
         }, completion: nil)
     }
     
-    // 真正的视图转场动画
+    // 真正的视图转场动画，没有共享元素
     func transAnimal2(){
         let otherView = UIView(frame: CGRect(x: 20, y: 100, width: 280, height: 300))
         otherView.backgroundColor = UIColor.blue
@@ -57,8 +123,8 @@ class ViewControllerAnimation: UIViewController {
         changeColor1sSpring()
         
         // 转场动画
-//        transAnimal1()
-        transAnimal2()
+        transAnimal1()
+//        transAnimal2()
     }
     
     func changeColor1sSpring(){
